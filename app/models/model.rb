4e610cfcda6346ba15886154cc045709b65a32c5
Oscar Vuong <oscarvuong@gmail.com>
2019-01-23 07:20:58 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,12 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+  Dogs = []
+  def initialize (name,breed,age)
+    @name = name
+    @breed = breed
+    @age = age
+    Dogs << self
+    
+  end
 
 end
